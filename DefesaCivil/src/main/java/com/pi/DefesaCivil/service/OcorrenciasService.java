5e366b9d1293731244bebbc5dd4410bd2263176d
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-03-26 21:51:11 -0300

    Update OcorrenciasService.java

--- DefesaCivil/src/main/java/com/pi/DefesaCivil/service/OcorrenciasService.java
+++ DefesaCivil/src/main/java/com/pi/DefesaCivil/service/OcorrenciasService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class OcorrenciaService {
+public class OcorrenciasService {
 
     private final UsuarioRepository usuarioRepository;
     private final OcorrenciasRepository ocorrenciasRepository;
