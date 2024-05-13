cbf59e1bb90894e8cc7f4ef121f880f03a170cdf
Juliana <julywhite.jw@gmail.com>
2024-05-12 22:04:48 -0300

    adicionando pasta de scritps manuais

---
+++ DefesaCivil/scripts/scripts_manuais.sql
@@ -0,0 +1,7 @@
+insert into administrador(cargo, email, login, nome_completo, senha) values ('Diretor geral', 'fulaninho@email.com', 'M5684', 'Fulano da Silva', 'teste123');
+
+select * from administrador ;
+
+delete from administrador where id_admin = 3;
+
+select * from usuario;
