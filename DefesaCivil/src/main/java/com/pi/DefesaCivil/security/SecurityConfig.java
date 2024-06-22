dc9d680ed6547329e27ec8df3ce97fd0a42ce369
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-21 23:05:25 -0300

    Atualizar o SecurityConfig.java
    adição de script da classe

--- DefesaCivil/src/main/java/com/pi/DefesaCivil/security/SecurityConfig.java
+++ DefesaCivil/src/main/java/com/pi/DefesaCivil/security/SecurityConfig.java
@@ -1,1 +1,17 @@
+package com.pi.DefesaCivil.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        
+        return new BCryptPasswordEncoder();
+    }
+    
+}
