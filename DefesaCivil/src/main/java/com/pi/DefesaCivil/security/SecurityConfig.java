54390e962042e63618f18c8bb899bc8c29705e8e
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-23 14:02:02 -0300

    Atualizar o SecurityConfig.java
    update na classe

--- DefesaCivil/src/main/java/com/pi/DefesaCivil/security/SecurityConfig.java
+++ DefesaCivil/src/main/java/com/pi/DefesaCivil/security/SecurityConfig.java
@@ -6,12 +6,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(auth -> auth
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults());
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        
         return new BCryptPasswordEncoder();
     }
-    
 }
